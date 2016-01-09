630f767bf2146056de7e8ccf88f0275bc9128201
Ramon Santos <ramonsantos@MacBook-Air-de-Ramon.local>
2016-01-09 10:54:31 -0300

    Criacao de controlador para envio de email, pagina de formulario ainda deve ser criada

--- src/despachante/controladores/EmailMBean.java
+++ src/despachante/controladores/EmailMBean.java
@@ -1,5 +1,40 @@
 package despachante.controladores;
 
-public class EmailMBean {
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
+import despachante.email.EmailService;
+import despachante.modelo.Email;
+
+@ManagedBean
+@RequestScoped
+public class EmailMBean {
+	private Email email;
+	private EmailService emailservice;
+
+	public EmailMBean() {
+		this.email = new Email();
+		this.emailservice = new EmailService();
+	}
+
+	public Email getEmail() {
+		return email;
+	}
+
+	public void setEmail(Email email) {
+		this.email = email;
+	}
+
+	public EmailService getEmailservice() {
+		return emailservice;
+	}
+
+	public void setEmailservice(EmailService emailservice) {
+		this.emailservice = emailservice;
+	}
+	
+	public void enviaEmail(){
+		EmailService.enviarEmail(this.email.getFrom(), this.email.getTo(), this.email.getAssunto(), this.email.getTexto());
+	}
+	
 }
--- src/despachante/modelo/Email.java
+++ src/despachante/modelo/Email.java
@@ -1,5 +1,38 @@
 package despachante.modelo;
 
 public class Email {
-
+	private String to;
+	private String from;
+	private String assunto;
+	private String texto;
+	public Email() {
+		this.to = new String();
+		this.from = new String();
+		this.assunto = new String();
+		this.texto = new String();
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getAssunto() {
+		return assunto;
+	}
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}	
 }
